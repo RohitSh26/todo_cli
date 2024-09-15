9a4ee89e32866cd7a09938a4cfb99942e08f6571
Rohit Sharma <rohit.sharma1048@gmail.com>
2024-09-15 00:57:37 -0500

    mutable keyword and a simple for loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+
+    // mutable keyword
+    let mut counter = 0;
+    
+    // for loop
+    for i in 1..=5{
+        counter += i;
+    }
+
+    println!("Counter {}", counter)
+
 }
