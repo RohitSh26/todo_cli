e301265e81327cf3d163379e9b26be1b7751761f
Rohit Sharma <rohit.sharma1048@gmail.com>
2024-09-15 01:08:15 -0500

    Implement a loop to interact with the user with if-else

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Todo CLI!");
 
-    // mutable keyword
-    let mut counter = 0;
-    
-    // for loop
-    for i in 1..=5{
-        counter += i;
+    loop {
+        println!("Please enter a command (add, list, quit):");
+
+        let mut command = String::new();
+
+        // read std input stream
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line.");
+
+        // trim read command
+        let command = command.trim();
+
+        // case when user quits
+        if command == "quit" {
+            println!("Goodbye!!");
+            break;
+        }
+        else {
+            println!("You entered {} Command", command);
+        }
     }
-
-    println!("Counter {}", counter)
 
 }
