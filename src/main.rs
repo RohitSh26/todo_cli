7c2eeaa485a0f568f284736be5baac41e506f942
Rohit Sharma <rohit.sharma1048@gmail.com>
2024-09-15 01:43:34 -0500

    add struct task,implement add, list, complete commands

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
 
+struct Task {
+    description: String,
+    completed: bool,
+}
+
 fn main() {
     println!("Welcome to Todo CLI!");
+
+    // create a tasks vector
+    let mut tasks: Vec<Task> = Vec::new();
 
     loop {
         println!("Please enter a command (add, list, quit):");
@@ -18,15 +26,69 @@ fn main() {
 
         // match keyword
         match command {
-            "add" => println!("You chose to add a new task."),
-            "list" => println!("Listing all tasks."),
+            "add" => {
+                println!("Enter task description:");
+                let mut description = String::new();
+
+                io::stdin()
+                    .read_line(&mut description)
+                    .expect("Fail to read description");
+
+                let description = description.trim().to_string();
+
+                let task = Task {
+                    description,
+                    completed: false,
+                };
+
+                tasks.push(task);
+
+                println!("Task added!!");
+            }
+            "list" => {
+                if tasks.is_empty() {
+                    println!("No task found.")
+                } else {
+                    println!("Your tasks:");
+                    for (index, task) in tasks.iter().enumerate() {
+                        let status = if task.completed { "✔" } else { "✘" };
+                        println!("{}: [{}] {}", index + 1, status, task.description)
+                    }
+                }
+            }
+            "complete" => {
+                if tasks.is_empty() {
+                    println!("No task found to complete.")
+                } else {
+                    println!("Enter the task # (number) of the task you have completed:");
+
+                    let mut input = String::new();
+
+                    io::stdin()
+                        .read_line(&mut input)
+                        .expect("Fail to read input");
+
+                    let input = input.trim();
+
+                    // convert input into a number
+                    match input.parse::<usize>() {
+                        Ok(num) => {
+                            if num == 0 || num > tasks.len() {
+                                println!("Invalid task number.");
+                            } else {
+                                tasks[num - 1].completed = true;
+                                println!("✔ Task {} marked as completed.", num);
+                            }
+                        }
+                        Err(_) => println!("Please enter a valid number."),
+                    }
+                }
+            }
             "quit" => {
                 println!("Goodbye!");
                 break;
             }
             _ => println!("Unknown command: {}", command),
         }
-        
     }
-
 }
